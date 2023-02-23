9407c38f66581cb4c7483171d4a4fdbc9eeefc6e
Gabie <67624149+gabizzle@users.noreply.github.com>
2023-02-22 21:11:33 -0500

    Update Lab 1 - Recon Automation.py

--- Lab 1 - Recon Automation.py
+++ Lab 1 - Recon Automation.py
@@ -1,5 +1,10 @@
 # Lab 1 - Recon Automation
 
+# The code is to retrieve or scrape YouTube videos. BeautifulSoup is used as the library because it can parse and obtain data from websites, and it is open-sourced. 
+# It begins by requesting the YouTube results. The code asks for a query and the next portion is where it gets the query you put in. 
+# Example is asking for a video about Cats. It will then base the search by the query (which is Cats). 
+# The results of the URLs with cats will then be stored. The URL will be ‘created’ or stored, and then it will return a video or list of videos and will be ‘printed’ out, which is the output of the code.
+
 import requests
 from bs4 import BeautifulSoup
 
